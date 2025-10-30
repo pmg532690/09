c32a9c1ff2e8975c51268e140cc9de673d538867
pmg532690 <pmg532690@sookmyung.ac.kr>
2025-10-30 15:56:45 +0900

    Update main.c

--- main.c
+++ main.c
@@ -5,22 +5,22 @@
 
 int main(int argc, char *argv[]) {
 	
-	int i, average;
-	int sum;
-	int grade [SIZE];
-	
-	sum = 0;
+	int i;
+	int grade[SIZE];
+	int score[SIZE];
 	
 	for(i=0;i<SIZE;i++)
+		grade[i] = rand() % 100; //0부터 99까지의 정수만 취함 
+	
+
+	for(i=0;i<SIZE;i++)
 	{
-		printf("학생 성적을 입력하세요: ");
-		scanf("%d", &grade[i]);
-		sum += grade[i]; 
+		grade[i] = score[i];
 	}
 	
-		
-	average = sum / SIZE;
-	printf("성적 평균 : %d\n", average);
-
+	for(i=0;i<SIZE;i++)
+		printf("grade[%d] = %d\n",i,score[i]);	
+	for(i=0;i<SIZE;i++)
+		printf("score[%d] = %d\n",i,score[i]);
 	return 0;
 }
