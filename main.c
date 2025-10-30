b367f66da6c331e195f6e9326ee1919ad0a23451
pmg532690 <pmg532690@sookmyung.ac.kr>
2025-10-30 15:18:23 +0900

    Create main.c

---
+++ main.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+/* run this program using the console pauser or add your own getch, system("pause") or input loop */
+
+int main(int argc, char *argv[]) {
+	
+	int testscore[5];
+	
+	return 0;
+}
