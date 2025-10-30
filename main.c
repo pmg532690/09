db3714c297dc07fa8f0b601ad4f8b06f719a6caa
pmg532690 <pmg532690@sookmyung.ac.kr>
2025-10-30 15:30:43 +0900

    Update main.c

--- main.c
+++ main.c
@@ -1,21 +1,20 @@
 #include <stdio.h>
 #include <stdlib.h>
-
+#define SIZE 5
 /* run this program using the console pauser or add your own getch, system("pause") or input loop */
 
 int main(int argc, char *argv[]) {
 	
-	int testscore[5];
 	int i;
+	int grade [SIZE];
+	printf("5명의 점수를 입력하세요.\n");
 	
-	testscore[0] = 10;
-	testscore[1] = 20;
-	testscore[2] = 30;
-	testscore[3] = 40;
-	testscore[4] = 50;
-	
-	for(i=0;i<5;i++)
-		printf("grade[%i] = %i\n", i, testscore[i]);
-			
+	for(i=0;i<SIZE;i++)
+		scanf("%d", &grade[i]);
+		
+	for(i=0;i<SIZE;i++)
+		printf("grade[%d] = %d\n",i,grade[i]);
+		
+
 	return 0;
 }
